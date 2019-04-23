3ebb108f4ea13891507032d37d0568cd36b9e08a
Ryan <ryan@axds.co>
2019-04-23 15:33:11 -0400

    Bump docs for 1.1.0

--- libpngpack/html/search/all_0.js
+++ libpngpack/html/search/all_0.js
@@ -3,6 +3,7 @@ var searchData=
   ['pngpack',['pngpack',['../structpngpack.html',1,'']]],
   ['pngpack_2eh',['pngpack.h',['../pngpack_8h.html',1,'']]],
   ['pngpack_5fadd_5fchannel',['pngpack_add_channel',['../pngpack_8h.html#a5ff85c1501d544d30813814d0a7dc2a2',1,'pngpack.h']]],
+  ['pngpack_5fadd_5ftextfield',['pngpack_add_textfield',['../pngpack_8h.html#a91216ba41fc4a0838e6e4d75614fb307',1,'pngpack.h']]],
   ['pngpack_5fbounds',['pngpack_bounds',['../structpngpack__bounds.html',1,'']]],
   ['pngpack_5fchannel',['pngpack_channel',['../structpngpack__channel.html',1,'']]],
   ['pngpack_5fchannel_5fadd_5ftextfield',['pngpack_channel_add_textfield',['../pngpack_8h.html#af7e0dc671c62cfd5d44c62a3b4a2044e',1,'pngpack.h']]],
--- libpngpack/html/search/functions_0.js
+++ libpngpack/html/search/functions_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['pngpack_5fadd_5fchannel',['pngpack_add_channel',['../pngpack_8h.html#a5ff85c1501d544d30813814d0a7dc2a2',1,'pngpack.h']]],
+  ['pngpack_5fadd_5ftextfield',['pngpack_add_textfield',['../pngpack_8h.html#a91216ba41fc4a0838e6e4d75614fb307',1,'pngpack.h']]],
   ['pngpack_5fchannel_5fadd_5ftextfield',['pngpack_channel_add_textfield',['../pngpack_8h.html#af7e0dc671c62cfd5d44c62a3b4a2044e',1,'pngpack.h']]],
   ['pngpack_5fchannel_5fnew',['pngpack_channel_new',['../pngpack_8h.html#ad6adf720058d0bcde017944fe7824d37',1,'pngpack.h']]],
   ['pngpack_5fnew',['pngpack_new',['../pngpack_8h.html#aa188dc6d71c02a1c43cffe8ca0b05765',1,'pngpack.h']]],
